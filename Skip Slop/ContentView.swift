d18175bd9775aa84f927c62978a459b6ba92b171
Ukaia <hello@nordictug.me>
2026-04-12 15:29:16 -0700

    Initial Commit

---
+++ Skip Slop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Skip Slop
+//
+//  Created by Ukaia Rogers on 4/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skip Slop/Skip_SlopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skip_SlopApp.swift
+//  Skip Slop
+//
+//  Created by Ukaia Rogers on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Skip_SlopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
